76ec762f17e908e8a8a9ebed26edab1cb36ca1e2
fakeef <fakeef55@gmail.com>
2022-09-08 00:23:38 +0900

    profile modification

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserInfoContext } from "./App";
 
-export default function Navigation() {
+export default function Navigation({ setIsNameChanged }: any) {
+  const userInfo = useContext(UserInfoContext);
+
   return (
     <ul>
       <li>
-        <Link to="/profile">Profile</Link>
+        <Link to="/profile"> Profile( {userInfo?.displayName} )</Link>
       </li>
     </ul>
   );
